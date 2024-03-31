bad6d7b9ff9dd29fa2756ca2a61b0c1925c0502b
pya1097 <pandurk@ncsu.edu>
2024-03-31 12:20:42 -0400

    edge server changes and user data install scheduler

--- host_NB/edge_abc_server.py
+++ host_NB/edge_abc_server.py
@@ -1,14 +1,15 @@
 from flask import Flask
 import requests
 import os
+from apscheduler.schedulers.background import BackgroundScheduler
+import time
 
 app = Flask(__name__)
 
 @app.route('/')
 def get_data():
-
     if not os.path.exists("data.txt"):     
-        url = "http://11.11.11.11:1234/get_data"
+        url = "http://1.1.1.2:1234/get_data"
         download_file(url, "data.txt")
 
     with open("data.txt", 'r') as file:
@@ -28,7 +29,22 @@ def download_file(url, filename):
     except Exception as e:
         print("An error occurred:", str(e))
 
+# Function to delete the file
+def delete_file():
+    file_path = "data.txt"
+    if os.path.exists(file_path):
+        os.remove(file_path)
+        print(f"{file_path} deleted successfully")
+    else:
+        print(f"{file_path} does not exist")
 
+# Initialize the scheduler
+scheduler = BackgroundScheduler()
+scheduler.add_job(delete_file, 'interval', seconds=60)  # Delete the file every hour
+scheduler.start()
 
 if __name__ == '__main__':
+    # Run Flask app
     app.run(host="0.0.0.0", port=8080)
+
+
