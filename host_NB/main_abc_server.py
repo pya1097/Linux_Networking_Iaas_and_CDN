23d1c99e250d65a47f79dbe0a3bb804793f0de51
RajatChandak2000 <rajatchandak2000@gmail.com>
2024-03-31 18:43:06 -0400

    Bug fixes 2

--- host_NB/main_abc_server.py
+++ host_NB/main_abc_server.py
@@ -1,19 +1,18 @@
-from flask import Flask
-import requests
 import os
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def get_data():
-    return "Hello world ABC"
+# Set the current working directory to the directory of the Python script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(script_dir)
 
 @app.route('/get_data')
 def send_file_data():
     with open('data.txt', 'r') as file:
-            file_content = file.read()
+        file_content = file.read()
     return file_content
-
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080)
+
