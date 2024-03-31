c838911c0b7b95fffb31afd7be134456966144b4
pya1097 <pandurk@ncsu.edu>
2024-03-31 18:49:58 -0400

    basic route

--- host_NB/main_abc_server.py
+++ host_NB/main_abc_server.py
@@ -3,6 +3,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def get_data():
+    return "Hello world from XYZ"
 # Set the current working directory to the directory of the Python script
 script_dir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(script_dir)
